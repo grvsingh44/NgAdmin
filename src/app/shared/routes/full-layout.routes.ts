005e9356f4f6e1c701d7714c1616e1193c93c42e
grvsingh44 <grv.singh4@gmail.com>
2018-03-28 20:27:36 +0530

    Chat Module routes added

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -6,4 +6,8 @@ export const FULL_ROUTES: Routes = [
     path: 'dashboard',
     loadChildren: './dashboard/dashboard.module#DashboardModule'
   },
+  {
+    path: 'chat',
+    loadChildren: './chat/chat.module#ChatModule'
+  },
 ];
