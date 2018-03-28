eef7db6b86e25fe54edd3e4bb86a4f35ed2ee6c9
grvsingh44 <grv.singh4@gmail.com>
2018-03-28 20:28:48 +0530

    Initial commit

---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+import { ChatRoutingModule } from './chat-routing.module';
+
+import { ChatComponent } from './chat.component';
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ChatRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
+  ],
+  declarations: [
+    ChatComponent
+  ]
+})
+export class ChatModule { }
