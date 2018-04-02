039a6c5a57116d582daea0f96c3ca18d2eefa742
Gaurav Singh <grv.singh4@gmail.com>
2018-04-03 01:27:44 +0530

    bug fix

--- src/assets/js/chat.js
+++ src/assets/js/chat.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
 	    $('.chat-application').height( $(window).height() - $('.navbar').height() - 100 );
 	});
 
-	$('.chat-app-content').height( $('.chat-app-window').height());
-
+	$('#chat-app-content').height( $('.chat-app-window').height())
+    .css({paddingLeft:10, paddingRight:10, position:'relative'});
 
 
 	if($('.chat-sidebar').length > 0){
 		$('.chat-sidebar').perfectScrollbar();
 	}
-  if($('.chat-app-content'). length > 0){
-	  $('.chat-app-content').perfectScrollbar();
+  if($('#chat-app-content'). length > 0){
+	  $('#chat-app-content').perfectScrollbar();
   }
 });
